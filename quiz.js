a8b7645276de19292138eb204fc48fc3976cf1a9
Daniella <apejidaniella@gmail.com>
2025-08-06 09:39:02 +0100

    add all files

---
+++ quiz.js
@@ -0,0 +1,15 @@
+function checkAnswer() {
+    const correctAnswer = "4";
+   const userAnswer = document.querySelector("input[name='quiz']:checked").value;
+    const feedback = document.getElementById("feedback")
+
+    if (correctAnswer === userAnswer) {
+        feedback.textContent = "Correct! Well done.";
+    } else {
+        feedback.textContent = "That's incorrect. Try again!";
+    }
+   
+}
+
+const submitButton = document.getElementById("submit-answer") 
+submitButton.addEventListener("click", checkAnswer) 
